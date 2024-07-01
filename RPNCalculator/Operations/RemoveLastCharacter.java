896166b5382a57b5cca68bd9a4e449b72587a175
dhthz <48757390+dhthz@users.noreply.github.com>
2024-07-02 00:55:18 +0300

    Delete RPNCalculator directory

--- RPNCalculator/Operations/RemoveLastCharacter.java
+++
@@ -1,9 +0,0 @@
-package Operations;
-
-public class RemoveLastCharacter {
-	public String removeLastChar(String s)   
-	{  
-	//returns the string after removing the last character  
-	return s.substring(0, s.length() - 1);  
-	}   
-}
